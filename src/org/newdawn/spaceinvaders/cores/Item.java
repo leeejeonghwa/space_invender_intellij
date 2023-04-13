586dc778877fcb4d3b2ce2d667e01fdb6a409c1c
Black-Stringer <hjs40111@gmail.com>
2023-04-13 17:11:58 +0900

    add comment

--- src/org/newdawn/spaceinvaders/cores/Item.java
+++ src/org/newdawn/spaceinvaders/cores/Item.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Item {
     private Boolean[] itemList = new Boolean[]{false, false, false, false, false};
-    //
+    //primitive 자료형 synchronized하려면 Atomic 자료형 필요
     private AtomicInteger money = new AtomicInteger(0);
 
     public void clearStage(Boolean[] itemState, AtomicInteger money){
