1963b9f6ebbb9aa09f4400f4a8aca9eb6b770c6c
Leejeonghwa <103178041+leeejeonghwa@users.noreply.github.com>
2023-05-26 18:16:01 +0900

    Revert "Refactoring"

--- src/org/newdawn/spaceinvaders/Item.java
+++ src/org/newdawn/spaceinvaders/Item.java
@@ -3,9 +3,48 @@ package org.newdawn.spaceinvaders;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Item {
-    public static Boolean[] gainedItems = new Boolean[]{false, false, false, false, false};
-    public static Boolean[] purchasedSkins = new Boolean[]{false, false, false, false, false};
+    private Boolean[] itemList = new Boolean[]{false, false, false, false, false};
+    private Boolean[] skinList = new Boolean[]{false, false, false, false, false};
     //primitive 자료형 synchronized하려면 Atomic 자료형 필요
-    public static AtomicInteger money = new AtomicInteger(0);
-    public static AtomicInteger activeSkinIndex = new AtomicInteger(-1);
+    private AtomicInteger money = new AtomicInteger(0);
+    private AtomicInteger activeSkin = new AtomicInteger(-1);
+
+    //setter of itemList
+    public void clearStage(Boolean[] itemState){
+        this.itemList = itemState;
+    }
+
+    //getter of itemList
+    public Boolean[] enableItems(){
+        return this.itemList;
+    }
+
+    //setter of money
+    public void setMoney(AtomicInteger money){
+        this.money.set(money.get());
+    }
+
+    //getter of money
+    public AtomicInteger getMoney(){
+        return this.money;
+    }
+
+    //setter of skinList
+    public void setEnableSkin(Boolean[] skinList){
+        this.skinList = skinList;
+    }
+
+    //getter of skinList
+    public Boolean[] enableSkinList(){
+        return this.skinList;
+    }
+
+    //setter of activate skin num
+    public void activateSkinNumber(AtomicInteger num){
+        this.activeSkin.set(num.get());
+    }
+
+    public AtomicInteger getActiveNum(){
+        return this.activeSkin;
+    }
 }
--- src/org/newdawn/spaceinvaders/MainWindow.java
+++ src/org/newdawn/spaceinvaders/MainWindow.java
@@ -6,18 +6,24 @@ import javax.swing.*;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainWindow extends JFrame {
+
+    private static Item item = new Item();
     
     private FirebaseTool firebaseTool;
 
     private GlobalStorage globalStorage;
 
     private JPanel panel;
-
-    //shopbtn, rulebtn, level1, level2, level3, level4, level5
-    private ArrayList<JButton> btnList = new ArrayList<>();
+    private JButton shopbtn;
+    private JButton rulebtn;
+    private JButton level1btn;
+    private JButton level2btn;
+    private JButton level3btn;
+    private JButton level4btn;
+    private JButton level5btn;
 
     public MainWindow() {
         // 메인 윈도우 설정
@@ -30,9 +36,8 @@ public class MainWindow extends JFrame {
         this.setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // 패널 생성, 버튼생성
+        // 패널 생성 및 시작 버튼 추가
         createPanel();
-        createButtons();
 
         // 메인 윈도우에 패널 추가
         getContentPane().add(panel);
@@ -45,44 +50,18 @@ public class MainWindow extends JFrame {
         globalStorage = GlobalStorage.getInstance();
     }
 
-    private void createPanel() {
-        // 패널 생성
-        panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Image backgorundImage = new ImageIcon("src/image/background.png").getImage(); // 이미지 파일
-                g.drawImage(backgorundImage, 0, 0, getWidth(), getHeight(), this);
-            }
-        };
-        panel.setLayout(null); // 레이아웃 매니저를 사용하지 않음
-        panel.setPreferredSize(new Dimension(800, 600));
-    }
-
-    private void createButtons() {
-        String[] srcList = new String[]{"shop", "rule", "level1", "level2", "level3", "level4", "level5"};
-        Integer[] xList = new Integer[]{460, 460, 630, 630, 630, 630, 630};
-        Integer[] yList = new Integer[]{410, 480, 200, 270, 340, 410, 480};
-
-        for (int i=0;i<7;i+=1){
-            btnList.add(drawButton("src/image/"+srcList[i]+".png", xList[i], yList[i]));
-            panel.add(btnList.get(i));
-            this.btnMouseListener(btnList.get(i));
-        }
-    }
-
-    private JButton drawButton(String ref, int x, int y){
+    private JButton drawButton(JButton button, String ref, int width, int height, int x, int y){
         ImageIcon buttonIcon = new ImageIcon(ref);
         Image buttonimg = buttonIcon.getImage();
-        Image buttonimgchange = buttonimg.getScaledInstance(150, 65, Image.SCALE_SMOOTH);
+        Image buttonimgchange = buttonimg.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         ImageIcon buttonchange = new ImageIcon(buttonimgchange);
-        JButton button = new JButton(buttonchange);
+        button = new JButton(buttonchange);
         button.setName(ref);
         button.setFocusPainted(false);
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
-        button.setSize(150, 65);
-        button.setBounds(x, y, 150, 65);
+        button.setSize(width, height);
+        button.setBounds(x, y, width, height);
 
         return button;
     }
@@ -90,25 +69,35 @@ public class MainWindow extends JFrame {
     public void btnMouseListener(JButton button){
         button.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                if (button.getName().equals("src/image/shop.png")){
+                if (button.getName() == "src/image/shop.png"){
                     button.setVisible(true);
                     setLayout(null);
                     Thread shopThread = new Thread(new Runnable() {
                         public void run() {
-                            ShopWindow s = new ShopWindow();
+                            System.out.print("shop Thread: " + Arrays.toString(item.enableItems())+ item.getMoney() + "\n");
+                            ShopWindow s = new ShopWindow(item.getMoney(), item.enableItems(), item.enableSkinList(), item.getActiveNum());
+                            s.shopLoop();
+                            synchronized(item){
+                                item.setMoney(s.recieveMoney());
+                                item.setEnableSkin(s.getEnableSkin());
+                                item.activateSkinNumber(s.getSelectedSkin());
+                            }
                         }
-                    }); shopThread.start();
+                    });
+                    shopThread.start();
                 }
-                else if (button.getName().equals("src/image/rule.png")){
+                else if (button.getName() == "src/image/rule.png"){
                     button.setVisible(true);
                     setLayout(null);
                     Thread ruleThread = new Thread(new Runnable() {
                         public void run() {
                             RuleWindow r = new RuleWindow();
+                            r.ruleLoop();
                         }
-                    }); ruleThread.start();
+                    });
+                    ruleThread.start();
                 }
-                else{
+                else if (button.getName() == "src/image/level1.png" || button.getName() == "src/image/level2.png" || button.getName() == "src/image/level3.png" || button.getName() == "src/image/level4.png" || button.getName() == "src/image/level5.png") {
                     // level 버튼 누른 경우
                     button.setVisible(true);
                     setLayout(null);
@@ -117,14 +106,67 @@ public class MainWindow extends JFrame {
                     // 게임 루프를 실행하는 스레드 생성
                     Thread gameThread = new Thread(new Runnable() {
                         public void run() {
-                            Game g = new Game(button.getName());
+                            System.out.print("game Thread: " + Arrays.toString(item.enableItems())+ item.getMoney() + "\n");
+                            Game g = new Game(button.getName(), item.enableItems(), item.getMoney(), item.getActiveNum());
                             g.gameLoop();
+                            synchronized(item){
+                                item.clearStage(g.getItemState());
+                                item.setMoney(g.recieveMoney());
+                            }
                         }
-                    }); gameThread.start();
+                    });
+                    gameThread.start();
                 }
 
             }
         });
+    }
+
+
+    private void createPanel() {
+        // 패널 생성
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon imageIcon = new ImageIcon("src/image/background.png"); // 이미지 파일
+                Image image = imageIcon.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        panel.setLayout(null); // 레이아웃 매니저를 사용하지 않음
+        panel.setPreferredSize(new Dimension(800, 600));
+
+        // shop 버튼 생성
+        shopbtn = drawButton(shopbtn, "src/image/shop.png", 150, 65, 460, 410);
+        this.btnMouseListener(shopbtn);
+        //설명 버튼 생성
+        rulebtn = drawButton(rulebtn, "src/image/rule.png", 150, 65, 460, 480);
+        this.btnMouseListener(rulebtn);
+        //level1 버튼 생성
+        level1btn = drawButton(level1btn, "src/image/level1.png", 150, 65, 630, 200);
+        this.btnMouseListener(level1btn);
+        //level2 버튼 생성
+        level2btn = drawButton(level2btn, "src/image/level2.png", 150, 65, 630, 270);
+        this.btnMouseListener(level2btn);
+        //level3 버튼 생성
+        level3btn = drawButton(level3btn, "src/image/level3.png", 150, 65, 630, 340);
+        this.btnMouseListener(level3btn);
+        //level4 버튼 생성
+        level4btn = drawButton(level4btn, "src/image/level4.png", 150, 65, 630, 410);
+        this.btnMouseListener(level4btn);
+        //level5 버튼 생성
+        level5btn = drawButton(level5btn, "src/image/level5.png", 150, 65, 630, 480);
+        this.btnMouseListener(level5btn);
+
+        // 패널에 시작 버튼 추가
+        panel.add(shopbtn);
+        panel.add(rulebtn);
+        panel.add(level1btn);
+        panel.add(level2btn);
+        panel.add(level3btn);
+        panel.add(level4btn);
+        panel.add(level5btn);
     }
 
     public static void main(String[] args) {
